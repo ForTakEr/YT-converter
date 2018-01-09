c740b5e6b4256263a51ebb281846da840d6e8fba
Gert-Andry <opilane@KLASSA302-13>
2018-01-09 11:23:24 +0200

    idee

--- Free Tärn YouTube converter/Form1.cs
+++ Free Tärn YouTube converter/Form1.cs
@@ -32,6 +32,22 @@ namespace Free_Tärn_YouTube_converter
         private void LinkBox_TextChanged(object sender, EventArgs e)
         {
             link = LinkBox.Text;
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"C:\Users\opilane\Documents\GitHub\YT-converter\Free Tärn YouTube converter\youtube-dl.exe",
+                    Arguments = "-F " + link,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+            }
         }
 
         private void Tõmba_Click(object sender, EventArgs e)
@@ -39,7 +55,10 @@ namespace Free_Tärn_YouTube_converter
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = @"C:\Users\opilane\Documents\GitHub\YT-converter\Free Tärn YouTube converter\youtube-dl.exe";
             startInfo.Arguments = "-f " + formaat + link;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
             Process.Start(startInfo);
+            MessageBox.Show("Video on convertitud");
         }
     }
 }
