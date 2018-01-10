7df518b608b07cbff9ba47a8428c6321d4ad4c8c
truberton <gert-andry.kaaramees@tptlive.ee>
2018-01-10 09:11:24 +0200

    Midagi on lappes

--- Free Tärn YouTube converter/Form1.cs
+++ Free Tärn YouTube converter/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 
 namespace Free_Tärn_YouTube_converter
 {
@@ -28,22 +29,7 @@ namespace Free_Tärn_YouTube_converter
 
         private void FormatList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            formaat = Convert.ToString(FormatList.SelectedItem);
-            //Video
-            if (formaat == "Mp4")
-            {
-                index = 137;
-            }
-            //Audio
-            if (formaat == "m4a")
-            {
-                index = 140;
-            }
-            //Audio
-            if (formaat == "webm")
-            {
-                index = 248;
-            }
+            
         }
 
         private void LinkBox_TextChanged(object sender, EventArgs e)
@@ -76,29 +62,104 @@ namespace Free_Tärn_YouTube_converter
 
         private void Tõmba_Click(object sender, EventArgs e)
         {
-            //If statementiga Tõmba ei tööta, kui ei ole kõik väljad täidetud
-            if (!string.IsNullOrWhiteSpace(link) && !string.IsNullOrWhiteSpace(formaat))
+            int i = 1;
+            formaat = Convert.ToString(FormatList.SelectedItem);
+            //Video
+            if (formaat == "Mp4")
             {
-                try
+                while (!File.Exists("*.mp4"))
                 {
-                    ProcessStartInfo startInfo = new ProcessStartInfo();
-                    startInfo.UseShellExecute = false;
-                    startInfo.CreateNoWindow = true;
-                    startInfo.FileName = "youtube-dl.exe";
-                    startInfo.Arguments = "-f " + index + " " + link;
-                    Process.Start(startInfo);
+                    switch (i)
+                    {
+                        case 1: //1080p
+                            index = 137;
+                            i++;
+                            break;
+                        case 2: //720p
+                            index = 22;
+                            i++;
+                            break;
+                        case 3: //480p
+                            index = 135;
+                            i++;
+                            break;
+                        case 4: //360p
+                            index = 18;
+                            i++;
+                            break;
+                    }
+                    if (!string.IsNullOrWhiteSpace(link) && !string.IsNullOrWhiteSpace(formaat))
+                    {
+                        var convert = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "youtube-dl.exe",
+                                Arguments = "-F " + link,
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = true
+                            }
+                        };
+                        try
+                        {
+                            convert.Start();
+                            kõik = convert.StandardOutput.ReadToEnd();
+                            convert.WaitForExit();
+
+                            if (File.Exists("*.mp4"))
+                            {
+                                break;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Youtube-dl ei leitud");
+                            throw;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Palun täida kõik väljad");
+                        break;
+                    }
                 }
-                catch (Exception)
-                {
-                    MessageBox.Show("Youtube-dl ei leitud");
-                    throw;
-                }
-                MessageBox.Show("Video on convertitud"); 
+                MessageBox.Show("Video on convertitud");
             }
-            else
+            //Audio
+            if (formaat == "m4a")
             {
-                MessageBox.Show("Palun täida kõik väljad");
+                index = 140;
             }
+            //Audio
+            if (formaat == "webm")
+            {
+                index = 248;
+            }
+
+            ////If statementiga Tõmba ei tööta, kui ei ole kõik väljad täidetud
+            //if (!string.IsNullOrWhiteSpace(link) && !string.IsNullOrWhiteSpace(formaat))
+            //{
+            //    try
+            //    {
+            //        ProcessStartInfo startInfo = new ProcessStartInfo();
+            //        startInfo.UseShellExecute = false;
+            //        startInfo.CreateNoWindow = true;
+            //        startInfo.FileName = "youtube-dl.exe";
+            //        startInfo.Arguments = "-f " + index + " " + link;
+            //        Process.Start(startInfo);
+            //    }
+            //    catch (Exception)
+            //    {
+            //        MessageBox.Show("Youtube-dl ei leitud");
+            //        throw;
+            //    }
+            //    MessageBox.Show("Video on convertitud"); 
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Palun täida kõik väljad");
+            //}
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
